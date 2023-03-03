0431b99fe16f75b1dc18a304cbc37ac8c3d8a166
hamzauguz <arastiranhamza@gmail.com>
2023-03-03 12:18:44 +0300

    create a cards folder and entegration

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
+import React from "react";
+import FirstlyCard from "./cards/firstly-card";
+import SecondlyCard from "./cards/secondly-card";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="main-card">
-        <div className="first-card">
-          <h4>Your Result</h4>
-          <p>Round</p>
-          <h5>Greate</h5>
-          <p>description</p>
-        </div>
-        <div className="secondly-card">
-          <h4>Summary</h4>
-          <div>cards</div>
-          <span>button</span>
-        </div>
+        <FirstlyCard />
+        <SecondlyCard />
       </div>
     </div>
   );
---
+++ src/cards/firstly-card/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Styles.FirstlyCard.css";
+
+const FirstlyCard = () => {
+  return (
+    <div className="first-card">
+      <h4>Your Result</h4>
+      <p>Round</p>
+      <h5>Greate</h5>
+      <p>description</p>
+    </div>
+  );
+};
+
+export default FirstlyCard;
