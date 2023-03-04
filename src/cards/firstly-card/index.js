897a72b77914bdb5de1ba7e09f3b46db739d9d65
hamzauguz <arastiranhamza@gmail.com>
2023-03-04 16:33:00 +0300

    full page finished

--- src/cards/firstly-card/index.js
+++ src/cards/firstly-card/index.js
@@ -7,8 +7,10 @@ const FirstlyCard = () => {
     <div className="first-card">
       <h3 className="result-text">Your Result</h3>
       <RoundPlace />
-      <h5>Greate</h5>
-      <p>You scored higher than 65% of the people who have taken there tests</p>
+      <h2 className="great-title">Great</h2>
+      <p className="description-card">
+        You scored higher than 65% of the people who have taken there tests
+      </p>
     </div>
   );
 };
